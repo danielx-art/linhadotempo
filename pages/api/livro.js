a9b88a863b9b563fa3b3048abef89437544741e7
danielx-art <metacarpo10@gmail.com>
2021-03-26 12:40:38 -0300

    march26

--- pages/api/livro.js
+++ pages/api/livro.js
@@ -1,8 +1,111 @@
-import livroJSON from '../../public/livro.json';
+//import livroJSON from '../../public/livro.json';
 
-function livro(req, res) {
-    res.json(livroJSON);
-    return;
+import fs from 'fs'
+
+const path = '../../public/livro.json';
+
+
+export default function getLivro() {
+
+    const livroRaw = [];
+    const formattedBook = [];
+
+    //1. if file doesnt exist, return empty response
+    if (!fs.existsSync(path)) {
+        res.json([]);
+    }
+
+    //2. load the file
+    const file = fs.readFileSync(path);
+    livroRaw = JSON.parse(usersRaw);
+
+    //3. FORMAT
+
+    //  -------------------NOTES -------------------
+    
+    //  -------------------DATES -------------------
+    //  the raw book comes in this format:
+    //  data:   [  [year, month, day] , [year, month, day ] ],
+    //  data2:  [  [year, month, day] , [year, month, day ] ]
+    //  I will format it to be:
+    //  dates = {
+    //      initial: [ {year, month, day, timestamp} , {year, month, day, timestamp} ],
+    //      final: [ {year, month, day, timestamp} , {year, month, day, timestamp} ]
+    //  }
+    //  
+    //  -------------------PERIODS-------------------
+    //  periods should get a period key, with an indent property
+    //  wich enables me to control horizontal positioning without
+    //  overlapping elements
+    //  period: {
+    //      indent
+    //  }
+    //  to do that, i use helper variables to track the current and
+    //  the previous period (default null) and the current indent used
+    let currentPeriod = null;
+    let previousPeriod = null;
+    let indent = 0;
+    //  ----------------------------------------------
+
+    // LOOP THROUGH THE BOOK:
+
+    for(let i=0; i<livroRaw.length; i++){
+
+        const li = livroRaw[i];                             //current entry in loop
+        const formattedEntry = {};
+        
+        //3.A. FORMAT DATES
+        const datesRaw = [];                                //this is just a helper array to loop wich stores the dates from the raw data
+        const dates = {initial: [], final: []};             //this is where the formatted dates will be
+        datesRaw.push(li.data);                             //push data
+        if(li.data2) datesRaw.push(li.data2);               //push data2
+
+        //loop through the raw dates
+        for(let j=0; j<datesRaw.length; j++){
+            //j refers to the date, if it is date1, then j=0, date2 then j=1
+            for(let k=0; k<datesRaw[j].length; k++){
+                //k refers to the dates of approximation in case date1 or date 2 have more then one date
+                const year = datesRaw[j][k][0]; //this is the year of the k-th approximation of date j on element i
+                const month = (datesRaw[j][k][1]) ? datesRaw[j][k][1] : null;
+                const day = (datesRaw[j][k][2]) ? datesRaw[j][k][2] : null;
+                const thisDate = new Date(year.toString(), month ? month.toString()-1 : "00", day ? day.toString() : "01");
+                const timestamp = thisDate.getTime();
+                const formattedDate = {
+                    year,
+                    month,
+                    day,
+                    timestamp
+                }
+                j == 0 ? dates.initial.push(formattedDate) : dates.final.push(formattedDate);
+            }
+        }
+        
+        //put dates in formattedEntry and also in current raw entry to facilitate
+        formattedEntry['dates'] = dates;
+        li['dates'] = dates;
+
+        //3.b FORMAT PERIODS
+        if(li.tipo == 'periodo') {
+
+            currentPeriod = li;
+
+            if(previousPeriod){
+                //test if it has a final date, if there is, compare, and if theres not, increment indent
+                if(previousPeriod.dates.final.length > 0) {
+                    //compare the bigger one
+                    if(previousPeriod.dates.final[previousPeriod.dates.final.length].timestamp > currentPeriod.dates.initial[0].timestamp){
+                        formattedEntry['period']
+                    }
+                }
+
+            } else {
+                //if theres no previous period then just initialize with indent 0
+                formattedEntry['period'] = {indent}
+            }
+
+        }   
+
+    }
+
+
 }
-
-export default livro;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,110 @@
 
 import dynamic from 'next/dynamic'
-const Personagem = dynamic(() => import('../comps/personagem'));
-const Periodo = dynamic(() => import('../comps/periodo.js'));
-const Evento = dynamic(() => import('../comps/evento.js'));
+import {useState, useEffect} from 'react'
 
+const Barra = dynamic(() => import('../comps/barra.js'));
 
 export async function getStaticProps(context) {
     
-    const livro = await import("../public/livro.json");
+    const livroData = await import("../public/livro.json");
+    const livroRaw = livroData.default;
+    
+    //Função factory que cria objeto livro
+    const createLivro = (raw) => {
+
+        /* how the element should be:
+
+        Year --
+        {
+            characters:
+        }
+
+
+
+        {
+            id,
+            type: "evento", "personagem" ou "periodo",
+            dates: {
+                initial: [{ year, month, day, timestamp}, //segunda aproximação {year, month, day timestamp}]
+                final: // mesma coisa que inicial
+            },
+            description: "...",
+            more: "..."
+            characters: ["...", "..."],
+            tags: ["...", "..."]
+        }
+        */
+
+        const byYears = [];
+
+        //LOOP TROUGH THE BOOK
+        for(let i=0; i<raw.length; i++){
+            //i refers to the book element in question
+            const li = raw[i];
+
+            //1.ORGANIZE THE DATES
+            const datesRaw = []; //this is just a helper array to loop
+            const dates = {initial: [], final: []}; //this is where the formatted dates will be
+            datesRaw.push(li.data);
+            if(li.data2) {
+                datesRaw.push(li.data2)
+            }
+            for(let j=0; j<datesRaw.length; j++){
+                //j refers to the date, if it is date1, then j=0, date2 then j=1
+                for(let k=0; k<datesRaw[j].length; k++){
+                    //k refers to the dates of approximation in case date1 or date 2 have more then one date
+                    const year = datesRaw[j][k][0]; //this is the year of the k-th approximation of date j on element i
+                    const month = (datesRaw[j][k][1]) ? datesRaw[j][k][1] : false;
+                    const day = (datesRaw[j][k][2]) ? datesRaw[j][k][2] : false;
+                    const dateobj = new Date(year.toString(), (month == true) ? month.toString()-1 : "00", (day == true) ? day.toString() : "01");
+                    const timestamp = dateobj.getTime();
+                    //console.log(timestamp); //debugg
+                    const formattedDate = {
+                        year,
+                        month,
+                        day,
+                        timestamp
+                    }
+                    j == 0 ? dates.initial.push(formattedDate) : dates.final.push(formattedDate);
+                }
+            }
+            
+            li['dates'] = dates;  //instead of putting this on li, create another new object entry for the new book
+
+            //2. ORGANIZE PERIODS INDEX
+            let previousPeriod = null;
+            let currentPeriod = null;
+            if(li.tipo == 'periodo') {
+                currentPeriod = li;
+
+                                
+            }
+
+        }//loop through raw book
+
+
+        const self = {
+            all: raw,
+            byYears
+        };
+        return self;
+    }
+
+    const livro = createLivro(livroRaw);
+
+    const addYear = (year) => {
+        return {
+            year,
+            events: [],
+            addEvent(obj) { return this.events.push(obj) },
+            getSize() { return this.events.length },
+        }
+    }
+
 
     return {
         props: {
-            livro: livro.default
+            livro: livroRaw
         }
     }
 }
@@ -20,46 +113,81 @@ function Home(props){
 
     /* here ill calculate a size of the div using a yearSize instead of fixing a height and using proportions */
     const livro = props.livro;
-    const inicialYear = livro[0].data[0][0];
+    const initialYear = livro[0].data[0][0];
     const finalYear = (livro[livro.length-1].data2 ? livro[livro.length-1].data2[0][0]: livro[livro.length-1].data[0][0]);
+    const lenghtYears = finalYear - initialYear;
     const yearSize = 12;
-    const containerL = (finalYear - inicialYear)*yearSize;
+    
+    const windowDimensions = useWindowSize();
+    const [zoomCheck, setZoomCheck] = useState(true);
+    const [zoom, setZoom] = useState(
+        windowDimensions.height/Math.max(lenghtYears*yearSize, 1)
+    );
+
+    const containerL = (lenghtYears*yearSize)*zoom/100;
+
+    function handleZoom(e) {
+        setZoom(e.currentTarget.value);
+    }
+
+    function handleCheckZoom(e) {
+        setZoomCheck(e.currentTarget.checked);
+        (e.currentTarget.checked == false) ? setZoom(windowDimensions.height/Math.max(lenghtYears*yearSize, 1)) : null
+    }
 
     return (
         <div className="out_container">
-            <div className="container">
-                {livro.map((el) => {
-                    return (
-                    <div className={el.tipo} key={el.id}>
-                        {el.tipo == 'personagem' && <Personagem obj={el}></Personagem>}
-                        Hello
-                    </div> )
-                })}
-                <div className="linha"></div>
+            <div className="zoom-options">
+                <input type="checkbox" name="zoom" checked={zoomCheck} onChange = {handleCheckZoom}></input>
+                <label htmlFor="zoom"> zoom </label>
+                <input 
+                type="range" min="1" max="100" value={zoom} className="slider" onInput = {handleZoom} disabled={!zoomCheck}></input>
             </div>
+            <Barra props={{livro, containerL, initialYear, finalYear, lenghtYears, yearSize}}></Barra>
             <style jsx>{`
                 .out_container{
                     position: relative;
                 }
-
-                .container {
-                    width: 100%;
-                    height: ${containerL}px;
-                    border: 1px solid blue
-                }
-
-                .linha {
-                    width: 2rem;
-                    height: 100%;
-                    background-color: red;
-                    position: absolute;
-                    top:0;
-                    left: 25%;
-                }
             `}</style>
-
         </div>
     )
 }
 
 export default Home;
+
+
+/* -------------------------------------------
+------ Hook for different windows sizes ------
+---------------------------------------------*/
+function useWindowSize() {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState({
+      width: 500,
+      height: 500,
+    });
+  
+    useEffect(() => {
+      // only execute all the code below in client side
+      if (typeof window !== 'undefined') {
+        // Handler to call on window resize
+        function handleResize() {
+          // Set window width/height to state
+          setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+          });
+        }
+      
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+       
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+      
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+      }
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
+  }
