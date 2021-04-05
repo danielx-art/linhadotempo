0b406e25c70bed0a62cfcb46d8f1614c2215382a
danielx-art <metacarpo10@gmail.com>
2021-04-05 11:42:26 -0300

    19

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export async function getStaticProps(context) {
 
     console.log(`The path is ${envPath + bookPath}`); //<-- This is outputting the absolute URL correctly
 
-    const bookTest = await fetch(envPath + bookPath).then(res => res.json());
+    const bookTest = await fetch(envPath + bookPath)
     const book = [];
 
     return {
